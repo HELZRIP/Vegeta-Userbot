da9a839b8f199d96560fb88ec252b1ab094385f6
Dream Garden (rey) <93763859+HELZRIP@users.noreply.github.com>
2021-12-07 15:34:54 +0700

    Update events.py

--- userbot/events.py
+++ userbot/events.py
@@ -97,8 +97,8 @@ def register(**args):
                 if not disable_errors:
                     date = strftime("%Y-%m-%d %H:%M:%S", gmtime())
 
-                    text = "**VEGETA-USERBOT ERROR**\n"
-                    link = "Silahkan chat: @FlashProSpeed"
+                    text = "**R-USERBOT ERROR**\n"
+                    link = "Silahkan chat: @helzrip"
                     text += "Untuk melaporkan kesalahan"
                     text += f"- tinggal teruskan pesan ini {link}.\n"
                     text += "Rama Siap Membantu Kamu\n"
